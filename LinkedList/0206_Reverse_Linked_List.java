96e5fbc99a60a6d0ad11b338e57fd0d52700a7ed
Arshiya H <arshiyahamsathali02@gmail.com>
2026-08-12 19:58:03 +0530

    Update 0206_Reverse_Linked_List.java

--- LinkedList/0206_Reverse_Linked_List.java
+++ LinkedList/0206_Reverse_Linked_List.java
@@ -1,5 +1,18 @@
 package Leetcode-java.LinkedList;
 
 public class 0206_Reverse_Linked_List {
-    
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+
+            ListNode next = current.next;
+
+            current.next = prev;
+
+            prev = current;
+            current = next;
+        }
+
+        return prev;
 }
