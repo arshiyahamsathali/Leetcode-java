c805ec9b4f2049fe3fc4944a2a1c2803cb0cd903
Arshiya H <arshiyahamsathali02@gmail.com>
2026-08-12 20:00:58 +0530

    Implement cycle detection in linked list

--- LinkedList/0141_Linked_List_Cycle.java
+++ LinkedList/0141_Linked_List_Cycle.java
@@ -1,5 +1,22 @@
 package Leetcode-java.LinkedList;
 
 public class 0141_Linked_List_Cycle {
+     public boolean hasCycle(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
     
 }
