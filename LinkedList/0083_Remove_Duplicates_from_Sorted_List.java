edf263bef433812f59007588f46fd236c86ed309
Arshiya H <arshiyahamsathali02@gmail.com>
2026-08-12 19:59:31 +0530

    Implement removal of duplicates from sorted list

--- LinkedList/0083_Remove_Duplicates_from_Sorted_List.java
+++ LinkedList/0083_Remove_Duplicates_from_Sorted_List.java
@@ -1,5 +1,16 @@
 package Leetcode-java.LinkedList;
 
 public class 0083_Remove_Duplicates_from_Sorted_List {
-    
+    ListNode current = head;
+
+    while (current != null && current.next != null) {
+
+        if (current.val == current.next.val) {
+            current.next = current.next.next;
+        } else {
+            current = current.next;
+        }
+    }
+
+    return head;
 }
